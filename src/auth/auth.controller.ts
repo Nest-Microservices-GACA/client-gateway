a1fd575c9c2e6a6f37bea98141d53b6472a0e88d
AngelMaga23 <angelmaga23@outlook.com>
2024-11-22 09:41:11 -0600

    Add Login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { AUTH_SERVICE, NATS_SERVICE } from 'src/config';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { catchError } from 'rxjs';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -13,6 +17,26 @@ export class AuthController {
       'auth.register.user',
       registerUserDto,
     );
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authClient.send('auth.login.user', loginUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+  }
+
+  @UseGuards( AuthGuard )
+  @Get('verify')
+  verifyToken( @User() user: CurrentUser, @Token() token: string  ) {
+
+    // const user = req['user'];
+    // const token = req['token'];
+
+    // return this.client.send('auth.verify.user', {});
+    return { user, token }
   }
   
 }
