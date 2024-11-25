f20a2e6fa9158821e8a0c91417617b04fb74dd06
AngelMaga23 <angelmaga23@outlook.com>
2024-11-25 15:47:26 -0600

    revert

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { RviaacModule } from './rviaac/rviaac.module';
 import { RviasaModule } from './rviasa/rviasa.module';
 import { RviacoModule } from './rviaco/rviaco.module';
 import { RviacpModule } from './rviacp/rviacp.module';
+<<<<<<< HEAD
 import { RviacalModule } from './rviacal/rviacal.module';
 import { RviadocModule } from './rviadoc/rviadoc.module';
 import { RviaprodocModule } from './rviaprodoc/rviaprodoc.module';
+<<<<<<< HEAD
 <<<<<<< HEAD
 import { RviamiModule } from './rviami/rviami.module';
 
@@ -16,6 +18,11 @@ import { RviamiModule } from './rviami/rviami.module';
 >>>>>>> parent of f309c6c (RVIACAL, RVIADOC, RVIAPRODOC)
 =======
 >>>>>>> parent of bddb525 (RVIACAL, RVIADOC, RVIAPRODOC)
+=======
+=======
+import { RviamiModule } from './rviami/rviami.module';
+>>>>>>> a24dfff4f7605f177f22ceab84e585bbcfa4bf10
+>>>>>>> parent of 405a4e7 (RVIACAL, RVIADOC, RVIAPRODOC)
 
 @Module({
   imports: [
@@ -28,10 +35,13 @@ import { RviamiModule } from './rviami/rviami.module';
 <<<<<<< HEAD
 <<<<<<< HEAD
     RviacpModule,
+<<<<<<< HEAD
     RviacalModule,
     RviadocModule,
-    RviaprodocModule,
+    RviaprodocModule
+=======
     RviamiModule
+<<<<<<< HEAD
 =======
     RviacpModule
 >>>>>>> parent of f309c6c (RVIACAL, RVIADOC, RVIAPRODOC)
@@ -41,6 +51,9 @@ import { RviamiModule } from './rviami/rviami.module';
     RviadocModule,
     RviaprodocModule
 >>>>>>> parent of bddb525 (RVIACAL, RVIADOC, RVIAPRODOC)
+=======
+>>>>>>> a24dfff4f7605f177f22ceab84e585bbcfa4bf10
+>>>>>>> parent of 405a4e7 (RVIACAL, RVIADOC, RVIAPRODOC)
   ],
 })
 export class AppModule {}
