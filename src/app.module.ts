405a4e78e365804274c015a5d655e01b8ca698bc
hechicero5000 <hechicero5000@gmail.com>
2024-11-25 14:49:30 -0600

    RVIACAL, RVIADOC, RVIAPRODOC

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,11 @@ import { RviaacModule } from './rviaac/rviaac.module';
 import { RviasaModule } from './rviasa/rviasa.module';
 import { RviacoModule } from './rviaco/rviaco.module';
 import { RviacpModule } from './rviacp/rviacp.module';
-<<<<<<< HEAD
 import { RviacalModule } from './rviacal/rviacal.module';
 import { RviadocModule } from './rviadoc/rviadoc.module';
 import { RviaprodocModule } from './rviaprodoc/rviaprodoc.module';
-=======
 import { RviamiModule } from './rviami/rviami.module';
->>>>>>> a24dfff4f7605f177f22ceab84e585bbcfa4bf10
+
 
 @Module({
   imports: [
@@ -23,13 +21,10 @@ import { RviamiModule } from './rviami/rviami.module';
     RviacoModule,
     AuthModule,
     RviacpModule,
-<<<<<<< HEAD
     RviacalModule,
     RviadocModule,
-    RviaprodocModule
-=======
+    RviaprodocModule,
     RviamiModule
->>>>>>> a24dfff4f7605f177f22ceab84e585bbcfa4bf10
   ],
 })
 export class AppModule {}
