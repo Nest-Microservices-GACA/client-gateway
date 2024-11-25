6d18fcb2de85646e49bf37169479ca85fd811f8c
AngelMaga23 <angelmaga23@outlook.com>
2024-11-25 17:51:01 -0600

    Change DTO rviami

--- src/rviami/dto/create-rviami.dto.ts
+++ src/rviami/dto/create-rviami.dto.ts
@@ -1,12 +1,10 @@
 import { Transform } from "class-transformer";
-import { IsIn, IsNumber } from "class-validator";
+import { IsIn, IsNumber, IsString } from "class-validator";
 
 export class CreateRviamiDto {
     @IsNumber()
     @Transform(({ value }) => parseInt(value, 10))
     idu_aplicacion: number;
-
-
 
     @IsNumber()
     @Transform(({ value }) => parseInt(value, 10))
@@ -14,4 +12,11 @@ export class CreateRviamiDto {
         message: 'El valor de num_accion debe ser 3',
     })
     num_accion: number;
+
+    @IsNumber()
+    @Transform(({ value }) => parseInt(value, 10))
+    numero_empleado: number;
+
+    @IsString()
+    path_project: string
 }
